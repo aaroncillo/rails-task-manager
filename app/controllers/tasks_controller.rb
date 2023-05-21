d3b3bc64106ffe307cfb5a327804a87be8281759
Aaron Deik <aaron.deik.rebora@gmail.com>
2023-05-21 16:36:40 -0400

    prueba esp32

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -5,6 +5,41 @@ class TasksController < ApplicationController
   end
 
   def show
-    @tasks = Task.find(params[id])
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to @task
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+    redirect_to task_path(@task)
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.delete
+    redirect_to tasks_path, status: :see_other
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,11 @@ Rails.application.routes.draw do
 
 
   #   /RUTA     CONTROLLER#ACCION
-  get "tasks/:id", to: "tasks#show", as: :task
-  get "tasks", to: "tasks#index"
+  delete 'tasks/:id', to: 'tasks#destroy'
+  patch 'tasks/:id', to: 'tasks#update'
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  post 'tasks', to: 'tasks#create'
+  get 'tasks/new', to: 'tasks#new', as: :new_task
+  get 'tasks/:id', to: 'tasks#show', as: :task
+  get 'tasks', to: 'tasks#index'
 end
